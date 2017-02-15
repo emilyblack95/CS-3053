e07b2e47b100c50fc23b5b6703c949556ff81b9f
Emily Black <emily.2014@yahoo.com>
2017-02-15 00:32:02 -0600

    Create Driver.java

---
+++ Assignment 2/Driver.java
@@ -0,0 +1,26 @@
+import Controller.Controller; 
+import Model.Model; 
+import View.View;
+
+/** 
+ * Driver for Assignment 2 - HCI
+ * 2/15/17
+ * @author Emily
+ * Navigation Ways:
+ * 1) Buttons
+ * 2) ComboBox
+ * 3) Keybindings (Control Left/Right arrow)
+ * 4) Click on images (Left/Right mouse button)
+ * 5) Slideshow
+ */
+
+public class Driver 
+{
+	public static void main(String[] args) 
+	{
+		Model m = new Model();
+		Controller c = new Controller(m);
+		View v = new View(m, c);
+		v.setVisible(true);
+	}
+}
